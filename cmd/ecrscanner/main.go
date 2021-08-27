98fa7b0b307262f0cde0ea3a03a358173ba597b8
Zack Brenton <zack@imposium.com>
2021-08-27 10:39:31 -0300

    add appName/version variables

--- cmd/ecrscanner/main.go
+++ cmd/ecrscanner/main.go
@@ -7,11 +7,15 @@ import (
 	"kube-ecr-image-scanner/internal/app/ecrscanner"
 )
 
+var appName = "kube-ecr-image-scanner"
+var version = "0.0.1-alpha.0"
+
 func main() {
+	klog.Infof("Starting %s:%s", appName, version)
 	cfg := &cmd.Config{}
 	arg.MustParse(cfg)
 	if err := ecrscanner.Run(cfg); err != nil {
-		klog.Errorf("Error running kube-ecr-image-scanner: %v", err)
+		klog.Errorf("Error running %s: %v", appName, err)
 	}
 	klog.Info("Shutting down...")
 }
